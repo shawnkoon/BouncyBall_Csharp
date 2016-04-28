26fea00e382f0f099fa192f1e49da92ceba512ff
shawnkoon <shawn9405@outlook.com>
2016-04-27 21:03:17 -0700

    Bouncy Ball added!

---
+++ wf_BouncyBall/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace wf_BouncyBall
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.playground = new System.Windows.Forms.Panel();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.racket = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.score_lbl = new System.Windows.Forms.Label();
+            this.helpWindow = new System.Windows.Forms.Label();
+            this.gameOver = new System.Windows.Forms.Label();
+            this.playground.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.racket)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // playground
+            // 
+            this.playground.Controls.Add(this.gameOver);
+            this.playground.Controls.Add(this.helpWindow);
+            this.playground.Controls.Add(this.score_lbl);
+            this.playground.Controls.Add(this.label1);
+            this.playground.Controls.Add(this.ball);
+            this.playground.Controls.Add(this.racket);
+            this.playground.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.playground.Location = new System.Drawing.Point(0, 0);
+            this.playground.Name = "playground";
+            this.playground.Size = new System.Drawing.Size(744, 420);
+            this.playground.TabIndex = 2;
+            this.playground.MouseEnter += new System.EventHandler(this.playground_MouseEnter);
+            this.playground.MouseLeave += new System.EventHandler(this.playground_MouseLeave);
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.Red;
+            this.ball.Location = new System.Drawing.Point(77, 62);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(20, 20);
+            this.ball.TabIndex = 3;
+            this.ball.TabStop = false;
+            // 
+            // racket
+            // 
+            this.racket.BackColor = System.Drawing.Color.Black;
+            this.racket.Location = new System.Drawing.Point(142, 300);
+            this.racket.Name = "racket";
+            this.racket.Size = new System.Drawing.Size(150, 15);
+            this.racket.TabIndex = 2;
+            this.racket.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Minecraft", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 35);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Score: ";
+            // 
+            // score_lbl
+            // 
+            this.score_lbl.AutoSize = true;
+            this.score_lbl.Font = new System.Drawing.Font("Minecraft", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.score_lbl.Location = new System.Drawing.Point(160, 9);
+            this.score_lbl.Name = "score_lbl";
+            this.score_lbl.Size = new System.Drawing.Size(0, 35);
+            this.score_lbl.TabIndex = 5;
+            // 
+            // helpWindow
+            // 
+            this.helpWindow.AutoSize = true;
+            this.helpWindow.Font = new System.Drawing.Font("Minecraft", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.helpWindow.Location = new System.Drawing.Point(436, 187);
+            this.helpWindow.Name = "helpWindow";
+            this.helpWindow.Size = new System.Drawing.Size(276, 145);
+            this.helpWindow.TabIndex = 6;
+            this.helpWindow.Text = "F1 - Restart\r\n\r\nP   - Pause / Play\r\n\r\nEsc - Quit";
+            // 
+            // gameOver
+            // 
+            this.gameOver.AutoSize = true;
+            this.gameOver.Font = new System.Drawing.Font("Minecraft", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameOver.Location = new System.Drawing.Point(250, 85);
+            this.gameOver.Name = "gameOver";
+            this.gameOver.Size = new System.Drawing.Size(315, 48);
+            this.gameOver.TabIndex = 7;
+            this.gameOver.Text = "Game Over!";
+            this.gameOver.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(744, 420);
+            this.Controls.Add(this.playground);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bouncy Ball";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.playground.ResumeLayout(false);
+            this.playground.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.racket)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel playground;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.PictureBox racket;
+        private System.Windows.Forms.Label score_lbl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label gameOver;
+        private System.Windows.Forms.Label helpWindow;
+    }
+}
+
---
+++ wf_BouncyBall/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wf_BouncyBall
+{
+    public partial class Form1 : Form
+    {
+
+        public int vSpeed = 2;     //speed of going up or down
+        public int hSpeed = 2;     //speed of left or right
+        public int score = 0;
+
+        Random dice = new Random();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Bounds = Screen.PrimaryScreen.Bounds; // make it full screen
+            racket.Top = playground.Bottom - (playground.Bottom / 20); // set racket position
+            score_lbl.Text = score.ToString();
+            gameOver.Visible = false;
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            helpWindow.Visible = false;
+
+            racket.Left = Cursor.Position.X - (racket.Width / 2);
+
+            ball.Left += hSpeed;
+            ball.Top += vSpeed;
+            
+            if(ball.Bottom >= racket.Top && ball.Bottom <= racket.Bottom && ball.Left+(ball.Width/1.5) >= racket.Left && ball.Right-(ball.Width /1.5) <= racket.Right)
+            {
+                if (hSpeed < 0)
+                    hSpeed -= 1;
+                else
+                    hSpeed += 1;
+
+                vSpeed += 1;
+                vSpeed = -vSpeed;
+                if((ball.Left <= (racket.Left+(racket.Width/4))))
+                {
+                    if (hSpeed > 0)
+                        hSpeed = -hSpeed;
+                    else
+                        hSpeed +=1;
+                }
+                else if((ball.Right >= (racket.Right - (racket.Width / 4))))
+                {
+                    if (hSpeed < 0)
+                        hSpeed = -hSpeed;
+                    else
+                        hSpeed -= 1;
+                }
+                
+                score++;
+                score_lbl.Text = score.ToString();
+            }
+            if (ball.Left <= playground.Left)
+                hSpeed = -hSpeed;
+            if (ball.Right >= playground.Right)
+                hSpeed = -hSpeed;
+            if (ball.Top <= playground.Top)
+                vSpeed = -vSpeed;
+            if (ball.Bottom >= playground.Bottom)
+            {
+                timer1.Enabled = false;              // when the ball hits the ground.
+                gameOver.Visible = true;
+                helpWindow.Visible = true;
+            }
+
+        }// end of timer 1 method
+
+        private void playground_MouseEnter(object sender, EventArgs e)
+        {
+            Cursor.Hide();
+        }
+
+        private void playground_MouseLeave(object sender, EventArgs e)
+        {
+            Cursor.Show();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Escape)
+            {
+                Application.Exit();
+            }
+            if(e.KeyCode == Keys.P && gameOver.Visible == false)
+            {
+                if (timer1.Enabled == false)
+                    timer1.Enabled = true;
+                else
+                    timer1.Enabled = false;
+                helpWindow.Visible = true;
+            }
+            if(e.KeyCode == Keys.F1)
+            {
+                ball.Left = playground.Right / 2;
+                ball.Top = (playground.Bottom / 3);
+                vSpeed = 2;
+                int val = dice.Next(0, 2);
+                if(val == 1)
+                {
+                    hSpeed = 2;
+                }
+                else
+                {
+                    hSpeed = -2;
+                }
+                score = 0;
+                score_lbl.Text = "0";
+                helpWindow.Visible = false;
+                gameOver.Visible = false;
+
+                timer1.Enabled = true;
+            }
+        }
+        
+    }
+}
